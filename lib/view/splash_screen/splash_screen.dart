8945b6ef4a3e187e9cada623e34394d8d26c41df
angelElizabethBerly <angel8michelle@gmail.com>
2024-02-26 23:54:15 +0530

    first commit

---
+++ lib/view/splash_screen/splash_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone_application/view/select_account_screen/select_account_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 3)).then((value) {
+      Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (context) => SelectAccountScreen()));
+    });
+    super.initState();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Image.asset(
+        "assets/images/Icon.png",
+        scale: 1.5,
+      )),
+    );
+  }
+}
